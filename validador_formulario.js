e257bd6d13dc91d21cc6a43d97dd27fac75b553b
psnroberto <robertop.souzaneto@gmail.com>
2023-06-22 16:26:55 -0300

    CMT4 - Ajustes CSS e JS
    Alterada a palheta de cores em CSS. Ajustado o JS para que todos os métodos e funções fiquem dentro da classe "Formulario".

--- validador_formulario.js
+++ validador_formulario.js
@@ -1,57 +1,36 @@
 
-const ajusteFormDiv = document.querySelector('div.erro-formulario')
-const ajusteFormParagrafo = document.getElementsByClassName('erro-paragrafo')
-const inputs = document.getElementsByClassName('inputs')
-const nome = document.querySelector('input#nome');
-const sobrenome = document.querySelector('input#sobrenome');
-const cpf = document.querySelector('input#cpf');
-const usuario = document.querySelector('input#usuario');
-const senha = document.querySelector('input#senha');
-const repetirSenha = document.querySelector('input#repetir-senha');
-const btnEnviar = document.querySelector('input.btn-enviar')
-const btnLimpar = document.querySelector('input.btn-limpar')
-
-btnEnviar.addEventListener('click', enviaFormulario)
-btnLimpar.addEventListener('click', limparFormulario)
-cpf.addEventListener('blur', formataCPF)
-cpf.addEventListener('focus', voltaCPF)
-
-function formataCPF() {
-    cpf.value = cpf.value.replace(/\D+/g, '')
-    cpf.value = cpf.value.slice(0, 3) + '.' + cpf.value.slice(3, 6) + '.' + cpf.value.slice(6, 9) + '-' + cpf.value.slice(9, 11)
-}
-
-function voltaCPF() {
-    if (cpf.value === '') {
-        cpf.value === ''
-    } else {
-        cpf.value = cpf.value.replace(/\D+/g, '')
-    }
-}
-
-function enviaFormulario() {
-    if (form1.validaFormulario() === true) {
-        console.log('O formulário está apto a ser enviado!')
-    } else {
-        console.log('Ajuste as pendências do formulário antes de envia-lo!')
-    }
-}
-
-function limparFormulario() {
-    for (let i = 0; i <= inputs.length - 1; i++) {
-        inputs[i].value = ''
-    }
-    ajusteFormDiv.style.display = 'none'
-}
-
 class Formulario {
-    constructor(nome, sobrenome, cpf, usuario, senha, repetirSenha) {
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.cpf = cpf;
-        this.usuario = usuario;
-        this.senha = senha;
-        this.repetirSenha = repetirSenha
+    constructor() {
+        this.nome = document.querySelector('input#nome');
+        this.sobrenome = document.querySelector('input#sobrenome');
+        this.cpf = document.querySelector('input#cpf');
+        this.usuario = document.querySelector('input#usuario');
+        this.senha = document.querySelector('input#senha');
+        this.repetirSenha = document.querySelector('input#repetir-senha')
+        this.ajusteFormDiv = document.querySelector('div.erro-formulario')
+        this.ajusteFormParagrafo = document.getElementsByClassName('erro-paragrafo')
+        this.inputs = document.getElementsByClassName('inputs')
+        this.btnEnviar = document.querySelector('input.btn-enviar')
+        this.btnLimpar = document.querySelector('input.btn-limpar')
+
+        this.adicionaEventos()
+
+    }
+
+    adicionaEventos() {
+        this.cpf.addEventListener('blur', () => { this.formataCPF() })
+        this.cpf.addEventListener('focus', () => { this.voltaCPF() })
+        this.btnEnviar.addEventListener('click', () => { this.enviaFormulario() })
+        this.btnLimpar.addEventListener('click', () => { this.limparFormulario() })
+    }
+
+    formataCPF() {
+        this.cpf.value = this.cpf.value.replace(/\D+/g, '')
+        this.cpf.value = this.cpf.value.slice(0, 3) + '.' + this.cpf.value.slice(3, 6) + '.' + this.cpf.value.slice(6, 9) + '-' + this.cpf.value.slice(9, 11)
+    }
+
+    voltaCPF() {
+        this.cpf.value = this.cpf.value.replace(/\D+/g, '')
     }
 
     get cpfLimpo() {
@@ -101,7 +80,7 @@ class Formulario {
             this.verificaSequencia() === true) {
             return false
         } else if (this.novoCPF === this.cpfLimpo) {
-            ajusteFormParagrafo[0].style.display = 'none'
+            this.ajusteFormParagrafo[0].style.display = 'none'
             return true
         }
     }
@@ -111,7 +90,7 @@ class Formulario {
         if (testeUsuario.length !== 0) {
             return false
         } else {
-            ajusteFormParagrafo[1].style.display = 'none'
+            this.ajusteFormParagrafo[1].style.display = 'none'
             return true
         }
     }
@@ -120,7 +99,7 @@ class Formulario {
         if (this.usuario.value.length < 3 || this.usuario.value.length > 12) {
             return false
         } else {
-            ajusteFormParagrafo[2].style.display = 'none'
+            this.ajusteFormParagrafo[2].style.display = 'none'
             return true
         }
     }
@@ -129,7 +108,7 @@ class Formulario {
         if (this.senha.value.length < 6 || this.senha.value.length > 12) {
             return false
         } else {
-            ajusteFormParagrafo[3].style.display = 'none'
+            this.ajusteFormParagrafo[3].style.display = 'none'
             return true
         }
     }
@@ -138,20 +117,20 @@ class Formulario {
         if (this.senha.value !== this.repetirSenha.value) {
             return false
         } else {
-            ajusteFormParagrafo[4].style.display = 'none'
+            this.ajusteFormParagrafo[4].style.display = 'none'
             return true
         }
     }
 
     validaPreenchimento() {
-        for (let i = 0; i <= inputs.length - 1; i++) {
-            if (inputs[i].value === '' || inputs[i].value === 'undefined') {
+        for (let i = 0; i <= this.inputs.length - 1; i++) {
+            if (this.inputs[i].value === '') {
                 return false
             }
         }
-        for (let i = 0; i <= inputs.length - 1; i++) {
-            if (inputs[i].value !== '') {
-                ajusteFormParagrafo[5].style.display = 'none'
+        for (let i = 0; i <= this.inputs.length - 1; i++) {
+            if (this.inputs[i].value !== '') {
+                this.ajusteFormParagrafo[5].style.display = 'none'
                 return true
             }
         }
@@ -162,38 +141,53 @@ class Formulario {
         let validador = true
 
         if (this.validaCPF() === false) {
-            ajusteFormParagrafo[0].style.display = 'block'
+            this.ajusteFormParagrafo[0].style.display = 'block'
             validador = false
         }
         if (this.validaUsuarioCaracter() === false) {
-            ajusteFormParagrafo[1].style.display = 'block'
+            this.ajusteFormParagrafo[1].style.display = 'block'
             validador = false
         }
         if (this.validaUsuarioComprimento() === false) {
-            ajusteFormParagrafo[2].style.display = 'block'
+            this.ajusteFormParagrafo[2].style.display = 'block'
             validador = false
         }
         if (this.validaSenha() === false) {
-            ajusteFormParagrafo[3].style.display = 'block'
+            this.ajusteFormParagrafo[3].style.display = 'block'
             validador = false
         }
         if (this.validaRepetirSenha() === false) {
-            ajusteFormParagrafo[4].style.display = 'block'
+            this.ajusteFormParagrafo[4].style.display = 'block'
             validador = false
         }
         if (this.validaPreenchimento() === false) {
-            ajusteFormParagrafo[5].style.display = 'block'
+            this.ajusteFormParagrafo[5].style.display = 'block'
             validador = false
         }
 
         if (validador === false) {
-            ajusteFormDiv.style.display = 'block'
+            this.ajusteFormDiv.style.display = 'block'
         } else {
-            ajusteFormDiv.style.display = 'none'
+            this.ajusteFormDiv.style.display = 'none'
         }
 
         return validador
     }
+
+    enviaFormulario() {
+        if (this.validaFormulario() === true) {
+            console.log('O formulário está apto a ser enviado!')
+        } else {
+            console.log('Ajuste as pendências do formulário antes de envia-lo!')
+        }
+    }
+
+    limparFormulario() {
+        for (let i = 0; i <= this.inputs.length - 1; i++) {
+            this.inputs[i].value = ''
+        }
+        this.ajusteFormDiv.style.display = 'none'
+    }
 }
 
-const form1 = new Formulario(nome, sobrenome, cpf, usuario, senha, repetirSenha)
+const form1 = new Formulario()
