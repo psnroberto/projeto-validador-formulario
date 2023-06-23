3f1a680cc8e5bb639ab3dab113045e3783a10d37
psnroberto <robertop.souzaneto@gmail.com>
2023-06-23 16:28:21 -0300

    CMT5 - Ajustes HTML, CSS e JSS
    Pequenos ajustes HTML e CSS. Alterada a lógica do código em JS para que as mensagens de erro fossem criadas pelo script, e não no HTML.

--- validador_formulario.js
+++ validador_formulario.js
@@ -8,29 +8,12 @@ class Formulario {
         this.senha = document.querySelector('input#senha');
         this.repetirSenha = document.querySelector('input#repetir-senha')
         this.ajusteFormDiv = document.querySelector('div.erro-formulario')
-        this.ajusteFormParagrafo = document.getElementsByClassName('erro-paragrafo')
         this.inputs = document.getElementsByClassName('inputs')
         this.btnEnviar = document.querySelector('input.btn-enviar')
         this.btnLimpar = document.querySelector('input.btn-limpar')
 
         this.adicionaEventos()
 
-    }
-
-    adicionaEventos() {
-        this.cpf.addEventListener('blur', () => { this.formataCPF() })
-        this.cpf.addEventListener('focus', () => { this.voltaCPF() })
-        this.btnEnviar.addEventListener('click', () => { this.enviaFormulario() })
-        this.btnLimpar.addEventListener('click', () => { this.limparFormulario() })
-    }
-
-    formataCPF() {
-        this.cpf.value = this.cpf.value.replace(/\D+/g, '')
-        this.cpf.value = this.cpf.value.slice(0, 3) + '.' + this.cpf.value.slice(3, 6) + '.' + this.cpf.value.slice(6, 9) + '-' + this.cpf.value.slice(9, 11)
-    }
-
-    voltaCPF() {
-        this.cpf.value = this.cpf.value.replace(/\D+/g, '')
     }
 
     get cpfLimpo() {
@@ -69,6 +52,23 @@ class Formulario {
         return String(digito)
     }
 
+    adicionaEventos() {
+        this.cpf.addEventListener('blur', () => { this.formataCPF() })
+        this.cpf.addEventListener('focus', () => { this.voltaCPF() })
+        this.btnEnviar.addEventListener('click', () => { this.enviaFormulario() })
+        this.btnLimpar.addEventListener('click', () => { this.limparFormulario() })
+    }
+
+    formataCPF() {
+        this.cpf.value = this.cpf.value.replace(/\D+/g, '')
+        this.cpf.value = this.cpf.value.slice(0, 3) + '.' + this.cpf.value.slice(3, 6) + '.' + this.cpf.value.slice(6, 9) + '-' + this.cpf.value.slice(9, 11)
+    }
+
+    voltaCPF() {
+        this.cpf.value = this.cpf.value.replace(/\D+/g, '')
+    }
+
+
     verificaSequencia() {
         const verificaSequencia = this.novoCPF[0].repeat(this.novoCPF.length)
         if (verificaSequencia === this.novoCPF) return true
@@ -80,7 +80,6 @@ class Formulario {
             this.verificaSequencia() === true) {
             return false
         } else if (this.novoCPF === this.cpfLimpo) {
-            this.ajusteFormParagrafo[0].style.display = 'none'
             return true
         }
     }
@@ -90,7 +89,6 @@ class Formulario {
         if (testeUsuario.length !== 0) {
             return false
         } else {
-            this.ajusteFormParagrafo[1].style.display = 'none'
             return true
         }
     }
@@ -99,7 +97,6 @@ class Formulario {
         if (this.usuario.value.length < 3 || this.usuario.value.length > 12) {
             return false
         } else {
-            this.ajusteFormParagrafo[2].style.display = 'none'
             return true
         }
     }
@@ -108,7 +105,6 @@ class Formulario {
         if (this.senha.value.length < 6 || this.senha.value.length > 12) {
             return false
         } else {
-            this.ajusteFormParagrafo[3].style.display = 'none'
             return true
         }
     }
@@ -117,7 +113,6 @@ class Formulario {
         if (this.senha.value !== this.repetirSenha.value) {
             return false
         } else {
-            this.ajusteFormParagrafo[4].style.display = 'none'
             return true
         }
     }
@@ -130,45 +125,71 @@ class Formulario {
         }
         for (let i = 0; i <= this.inputs.length - 1; i++) {
             if (this.inputs[i].value !== '') {
-                this.ajusteFormParagrafo[5].style.display = 'none'
                 return true
             }
         }
+    }
+
+    criaDivTitulo(){
+        let divTitulo = document.createElement('h3')
+        divTitulo.innerText = 'Ajuste o seu formulário'
+        this.ajusteFormDiv.appendChild(divTitulo) 
+        return divTitulo
+    }
+
+    criaErro(mensagemErro) {
+        let criaErro = document.createElement('p')
+        criaErro.innerText = mensagemErro
+        criaErro.classList.add('aparece')
+        this.ajusteFormDiv.appendChild(criaErro)
+        return criaErro
     }
 
     validaFormulario() {
 
         let validador = true
 
-        if (this.validaCPF() === false) {
-            this.ajusteFormParagrafo[0].style.display = 'block'
-            validador = false
-        }
-        if (this.validaUsuarioCaracter() === false) {
-            this.ajusteFormParagrafo[1].style.display = 'block'
-            validador = false
-        }
-        if (this.validaUsuarioComprimento() === false) {
-            this.ajusteFormParagrafo[2].style.display = 'block'
-            validador = false
-        }
-        if (this.validaSenha() === false) {
-            this.ajusteFormParagrafo[3].style.display = 'block'
-            validador = false
-        }
-        if (this.validaRepetirSenha() === false) {
-            this.ajusteFormParagrafo[4].style.display = 'block'
-            validador = false
-        }
+        this.ajusteFormDiv.innerHTML = ''
+        this.criaDivTitulo()
+
         if (this.validaPreenchimento() === false) {
-            this.ajusteFormParagrafo[5].style.display = 'block'
+            if (this.criaErro('Todos os campos do formulário devem ser preenchidos.').classList.contains('aparece')) { this.criaErro().remove() }
+            validador = false
+        }
+
+        if (this.validaCPF() === false) {
+            if (this.criaErro('Deve ser informado um número de CPF válido.').classList.contains('aparece')) 
+            { this.criaErro().remove() }
+            validador = false
+        }
+
+        if (this.validaUsuarioCaracter() === false) {
+            if (this.criaErro('O campo "usuário" deve conter apenas letras e/ou números.').classList.contains('aparece')) { this.criaErro().remove() }
+            validador = false
+        }
+
+        if (this.validaUsuarioComprimento() === false) {
+            if (this.criaErro(' O campo "usuário" deve conter entre 3 e 12 caracteres.').classList.contains('aparece')) { this.criaErro().remove() }
+            validador = false
+        }
+ 
+        if (this.validaSenha() === false) {
+            if (this.criaErro('O campo "senha" deve conter entre 6 e 12 caracteres.').classList.contains('aparece')) { this.criaErro().remove() }
+            validador = false
+        }
+  
+        if (this.validaRepetirSenha() === false) {
+            if (this.criaErro('As senhas informadas devem ser iguais.').classList.contains('aparece')) 
+            { this.criaErro().remove() }
             validador = false
         }
 
         if (validador === false) {
-            this.ajusteFormDiv.style.display = 'block'
+            this.ajusteFormDiv.classList.remove('desaparece')
+            this.ajusteFormDiv.classList.add('aparece')
         } else {
-            this.ajusteFormDiv.style.display = 'none'
+            this.ajusteFormDiv.classList.remove('aparece')
+            this.ajusteFormDiv.classList.add('desaparece')
         }
 
         return validador
@@ -186,7 +207,8 @@ class Formulario {
         for (let i = 0; i <= this.inputs.length - 1; i++) {
             this.inputs[i].value = ''
         }
-        this.ajusteFormDiv.style.display = 'none'
+        this.ajusteFormDiv.classList.remove('aparece')
+        this.ajusteFormDiv.classList.add('desaparece')
     }
 }
 
